df7c2913ae2710dcc81c9493cd7564e5e7aeaad4
vaibhav bachate <168278464+Vaibhav8855@users.noreply.github.com>
2025-04-11 18:19:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,81 @@
-hiii this is file made for demo.
+  apt install git -y
+    2  mkdir git
+    3  git init
+    4  git config --global user.name vaibhav
+    5  git config --global user.email vaibhav123@gmail.com
+    6  touch file1.py
+    7  vi file1.py
+    8  git add file1.py 
+    9  git status
+   10  git rm --cached file1.py 
+   11  git status
+   12  ls
+   13  git add file1.py 
+   14  git log
+   15  git commit -m "new feature"
+   16  git status
+   17  git log
+   18  git show
+   19  vi file1.py
+   20  git add file1.py 
+   21  git commit -m "new feature updated"
+   22  git show
+   23  git remote -v
+   24  git remote add origin https://github.com/Vaibhav8855/AWS.git
+   25  git remote -v
+   26  git branch -M main
+   27  git push -u origin main
+   28  git push -u origin master
+   29  git pull https://github.com/Vaibhav8855/AWS.git
+   30  git push -u origin main
+   31  git branch -M master
+   32  git push -u origin main
+   33  git push -u origin master
+   34  git add .
+   35  git commit -m "new feature updated2"
+   36  git push -u origin main
+   37  git push -u origin master
+   38  git fetch
+   39  git push -u origin master
+   40  git pull origin master --rebase
+   41  git push -u origin master
+   42  history
+   43  git clone https://github.com/Vaibhav8855/CDEC-13-Devops.git
+   44  ls
+   45  cd CDEC-13-Devops/
+   46  ls
+   47  touch main.py
+   48  git status
+   49  git  add main.py
+   50  la
+   51  git  commit "demo commit"
+   52  git  commit -m "demo commit"
+   53  git pull https://github.com/Vaibhav8855/CDEC-13-Devops.git
+   54  git remote -v
+   55  git push -u origin main
+   56  history
+   57  git fetch https://github.com/Vaibhav8855/CDEC-13-Devops.git
+   58  ls
+   59  touch feature.py
+   60  git pull https://github.com/Vaibhav8855/CDEC-13-Devops.git
+   61  cat main.py
+   62  git add .
+   63  git commit -m "new feature"
+   64  git remote -v
+   65  git push -u origin main
+   66  git branch
+   67  git branch -b feature
+   68  git branch  feature
+   69  git branch
+   70  ls
+   71  git checkout -b feature
+   72  git checkout -b fix
+   73  git checkout  feature
+   74  ls
+   75  touch demo
+   76  git checkout main
+   77  ls
+   78  git checkout fix
+   79  ls
+   80  history
+root@ip-172-31-85-247:/home/ubuntu/CDEC-13-Devops# 
